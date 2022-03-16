9594659a2198c4ec94259c54ea1eaae8bc8f754d
Liane Nakamura <lianenakamura@gmail.com>
2022-03-15 20:45:27 -0700

    Homepage layout updates; rebrand to Sesame Prize

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
             <LinkContainer to="/">
-              <Navbar.Brand>DFC</Navbar.Brand>
+              <Navbar.Brand>Sesame Prize</Navbar.Brand>
             </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
