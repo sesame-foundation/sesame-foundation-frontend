1c9a9ac4968cd01df0dcf141efefc2c8286fcf40
Liane Nakamura <lianenakamura@gmail.com>
2022-03-31 10:39:27 -0700

    Show that the challenge has been solved

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## Running in development mode
 
 1. Follow the instructions in the REAMDE of decentralized-factoring-challenge-backend to start a local testnet and deploy a factoring challenge contract.
-2. Update `src.utils.js` with the address of your deployed contract.
+2. Update `src/contracts/network-config.json` with the address of your deployed contract.
 3. Run `npm start`.
--- src/components/FactoringChallenge.js
+++ src/components/FactoringChallenge.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { providerContract, provider } from "../utils.js";
 import { DonateButton } from "./DonateButton";
 import { SolutionSubmissionButton } from "./SolutionSubmissionButton";
+import { SolvedBadge } from "./SolvedBadge";
 import { ethers } from "ethers";
-import Badge from "react-bootstrap/Badge";
+
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
@@ -12,117 +13,127 @@ import "./FactoringChallenge.css";
 const bn = require("bn.js");
 const MAX_DIGITS = 10;
 
-function copyToClipboard(content) {
-  var sampleTextarea = document.createElement("textarea");
-  document.body.appendChild(sampleTextarea);
-  sampleTextarea.value = content;
-  sampleTextarea.select();
-  document.execCommand("copy");
-  document.body.removeChild(sampleTextarea);
-}
+export function FactoringChallenge() {
+  const [product, setProduct] = useState(null);
+  const [withdrawlDelay, setWithdrawlDelay] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [winner, setWinner] = useState(null);
 
-export class FactoringChallenge extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { product: null, withdrawlDelay: null, balance: null };
-  }
+  const isUnsolved = winner == null || /^0x0+$/.test(winner);
 
-  setProduct(product) {
-    this.setState({ product: product.toString() });
-  }
+  const copyToClipboard = (content) => {
+    var sampleTextarea = document.createElement("textarea");
+    document.body.appendChild(sampleTextarea);
+    sampleTextarea.value = content;
+    sampleTextarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(sampleTextarea);
+  };
 
-  setWithdrawlDelay(withdrawlDelay) {
-    this.setState({ withdrawlDelay: withdrawlDelay.toString() });
-  }
-
-  setBalance(balance) {
-    this.setState({ balance: ethers.utils.formatEther(balance) });
-  }
-
-  getFormattedProduct() {
-    if (this.state.product) {
-      return this.state.product.length > MAX_DIGITS
-        ? this.state.product.substring(0, MAX_DIGITS) + "..."
-        : this.state.product;
+  const getFormattedProduct = () => {
+    if (product) {
+      return product.length > MAX_DIGITS
+        ? product.substring(0, MAX_DIGITS) + "..."
+        : product;
     } else {
       return "";
     }
-  }
+  };
 
-  handleCopy() {
-    copyToClipboard(this.state.product);
-  }
+  const updateBalance = () => {
+    provider
+      .getBalance(providerContract.address)
+      .then((balance) => {
+        const formattedBalance = ethers.utils.formatEther(balance);
+        setBalance(formattedBalance);
+      });
+  };
 
-  render() {
-    return (
-      <Container className="factoring-challenge">
-        <Row className="px-4 my-5">
-          <Col>
-            <h1 className="display-5 fw-bold">
-              Decentralized Factoring Challenge
-            </h1>
-            <p className="lead">
-              A decentralized factoring challenge to encourage research in
-              computational number theory.
-            </p>
-            <Badge bg="secondary">
-              Status: Unsolved
-            </Badge>
-          </Col>
-        </Row>
-        <Row>
-          <Col md className="my-5">
-            <h2 className="h4 mb-0">
-              Product
-            </h2>
+  const updateProduct = () => {
+    providerContract
+      .product()
+      .then((product) => {
+        const truncatedProduct = new bn(product[0].slice(2), 16).toString(10);
+        setProduct(truncatedProduct.toString());
+      });
+  };
 
-            <p className="product">
-              {this.getFormattedProduct()}
-            </p>
-            <div
-              style={{ display: "flex", gap: "10px", justifyContent: "center" }}
-            >
+  const updateWinner = () => {
+    providerContract
+      .winner()
+      .then((winner) => setWinner(winner.toString()));
+  };
+
+  const updateWithdrawlDelay = () => {
+    providerContract
+      .withdrawlDelay()
+      .then((withdrawlDelay) => setWithdrawlDelay(withdrawlDelay.toString()));
+  };
+
+  const updateSolvedStates = () => {
+    updateBalance();
+    updateWinner();
+  };
+
+  useEffect(() => {
+    updateBalance();
+    updateProduct();
+    updateWinner();
+    updateWithdrawlDelay();
+  });
+
+  return (
+    <Container className="factoring-challenge">
+      <Row className="px-4 my-5">
+        <Col>
+          <h1 className="display-5 fw-bold">
+            Decentralized Factoring Challenge
+          </h1>
+          <p className="lead">
+            A decentralized factoring challenge to encourage research in
+            computational number theory.
+          </p>
+          <SolvedBadge isUnsolved={isUnsolved} />
+        </Col>
+      </Row>
+      <Row>
+        <Col md className="my-5">
+          <h2 className="h4 mb-0">
+            Product
+          </h2>
+
+          <p className="product">
+            {getFormattedProduct()}
+          </p>
+          {isUnsolved &&
+            <div className="product-buttons">
               <Button
                 variant="primary"
-                onClick={() => this.handleCopy()}
+                onClick={() => copyToClipboard(product)}
                 title="Copy to Clipboard"
               >
                 Copy
               </Button>
+
               <SolutionSubmissionButton
-                withdrawalDelay={this.state.withdrawlDelay}
-                onSubmitSolution={() => this.updateBalance()}
+                withdrawalDelay={withdrawlDelay}
+                onSubmitSolution={updateSolvedStates}
               />
             </div>
-          </Col>
-          <Col md className="my-5">
-            <h2 className="h4 mb-0">
-              ETH Prize
-            </h2>
-            <p className="product">
-              {this.state.balance}
-            </p>
-            <DonateButton onDonate={() => this.updateBalance()} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
-  updateBalance() {
-    provider
-      .getBalance(providerContract.address)
-      .then((balance) => this.setBalance(balance));
-  }
-
-  componentDidMount() {
-    providerContract.product().then((product) => {
-      this.setProduct(new bn(product[0].slice(2), 16).toString(10));
-    });
-
-    providerContract
-      .withdrawlDelay()
-      .then((withdrawlDelay) => this.setWithdrawlDelay(withdrawlDelay));
-    this.updateBalance();
-  }
+          }
+        </Col>
+        <Col md className="my-5">
+          <h2 className="h4 mb-0">
+            ETH Prize
+          </h2>
+          <p className="product">
+            {balance}
+          </p>
+          {isUnsolved &&
+            <DonateButton onDonate={updateBalance} />
+          }
+        </Col>
+      </Row>
+    </Container>
+  );
 }
