9156a5e0acdeae8885616c962a98cb69c7038934
Max Radin <radin.max@gmail.com>
2022-04-23 14:45:06 -0400

    Adding default provider

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
 import { ethers } from "ethers";
-import networkConfig from "./contracts/network-config.json"
+import networkConfig from "./contracts/network-config.json";
 
 const chainId = process.env.REACT_APP_DEFAULT_CHAIN_ID;
 const contractName = "MyContract";
 const contractAddress = networkConfig[chainId][contractName];
 const contractDescription = require(`./contracts/${contractName}.json`);
 
-export const provider = new ethers.providers.Web3Provider(window.ethereum);
+export const provider = window.ethereum
+  ? new ethers.providers.Web3Provider(window.ethereum)
+  : ethers.getDefaultProvider();
 
 export const providerContract = new ethers.Contract(
   contractAddress,
@@ -20,9 +22,5 @@ export async function getSigner() {
 }
 
 export function getSignerContract(signer) {
-  return new ethers.Contract(
-    contractAddress,
-    contractDescription.abi,
-    signer
-  );
+  return new ethers.Contract(contractAddress, contractDescription.abi, signer);
 }
